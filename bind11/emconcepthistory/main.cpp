822d762b33343a272b68eb66c386729cb92dfc4a
Jimmy M. Gong <jimmy.m.gong@gmail.com>
2020-11-30 19:28:50 +0800

    python binding, easymoney concept history

---
+++ bind11/emconcepthistory/main.cpp
@@ -0,0 +1,83 @@
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include <QtCore/QObject>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <typeinfo>
+#include <vector>
+
+#include "abquant/actions/abquant.hpp"
+#include "abquant/actions/emconcepthistory.hpp"
+#include "abquant/actions/utils.hpp"
+
+// using namespace std;
+namespace abq
+{
+class PyEmConceptHistory
+{
+public:
+    PyEmConceptHistory(std::vector<std::string> codes, const string& start, const string& end)
+        : m_codes{codes}, m_start{start}, m_end{end}
+    {
+        QStringList qcodes;
+        for (auto c : codes) {
+            qcodes << QString::fromStdString(c);
+        }
+        m_cda = EmConceptHistoryAction(qcodes, m_start.c_str(), m_end.c_str());
+        m_cda.setDataFrame();
+    };
+
+    template <class T>
+    std::vector<T> toSeries(const string& col) const noexcept
+    {
+        if constexpr (std::is_same_v<T, double>) {
+            auto series = m_cda.get_pyseries(col.c_str());
+            return series;
+        }
+
+        auto series = m_cda.toSeries<T>(col.c_str());
+        return series.toStdVector();
+    }
+    ~PyEmConceptHistory() = default;
+
+private:
+    std::vector<std::string> m_codes{};
+    const string m_start{};
+    const string m_end{};
+    EmConceptHistoryAction m_cda{};
+};
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(abqemconcepthistory, m)
+{
+    m.doc() = R"pbdoc(
+        Pybind11 EmConceptHistory plugin
+        -----------------------
+
+        .. abqemconcepthistory:: currentmodule_exmaple
+
+        .. autosummary::
+           :emconcepthistory: toSeries
+
+           toSeries
+    )pbdoc";
+
+    py::class_<PyEmConceptHistory> sm_class(m, "PyEmConceptHistory");
+    sm_class.def(py::init<std::vector<std::string>, const string, const string>())
+        .def("toSeries", &PyEmConceptHistory::toSeries<double>,
+             R"pbdoc(toSeries double toSeries double function.)pbdoc")
+        .def("toSeries_string", &PyEmConceptHistory::toSeries<std::string>,
+             R"pbdoc(toSeries string toSeries string function.)pbdoc");
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = VERSION_INFO;
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
+
+} // namespace abq
---
+++ bind11/test/test_emconcepthistory.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import pandas as pd
+
+from abqemconcepthistory import PyEmConceptHistory as emconcepthistory
+from pyabquant import PyAbquant
+
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", None)
+
+
+class MainTest(unittest.TestCase):
+    def testDay(self):
+        codes = ["90.bk0980", "90.bk0990"]
+        start = "2019-01-01"
+        end = "2019-12-01"
+        sd = emconcepthistory(codes, start, end)
+
+        date = sd.toSeries_string("date")
+        open_ = sd.toSeries("open")
+        close = sd.toSeries("close")
+        high = sd.toSeries("high")
+        low = sd.toSeries("low")
+        volume = sd.toSeries("volume")
+        amount = sd.toSeries("amount")
+        amplitude = sd.toSeries("amplitude")
+        rise_fall_pct = sd.toSeries("rise_fall_pct")
+        rise_fall_amt = sd.toSeries("rise_fall_amt")
+        turnover = sd.toSeries("turnover")
+        mkt_code = sd.toSeries_string("mkt_code")
+        print(date[:10])
+        print(mkt_code[:10])
+        date_stamp = sd.toSeries("date_stamp")
+
+        df = pd.DataFrame(
+            {
+                "date": date,
+                "open": open_,
+                "close": close,
+                "high": high,
+                "low": low,
+                "volume": volume,
+                "amount": amount,
+                "amplitude": amplitude,
+                "rise_fall_pct": rise_fall_pct,
+                "rise_fall_amt": rise_fall_amt,
+                "turnover": turnover,
+                "mkt_code": mkt_code,
+                "date_stamp": date_stamp,
+            }
+        )
+        df.set_index(["mkt_code", "date"], inplace=True)
+
+        print(df[:10])
+        print(df[-10:])
+
+        self.assertTrue(len(open_) > 0)
+        self.assertAlmostEqual(open_[0], 559.62, delta=0.001)
+
+
+if __name__ == "__main__":
+    PyAbquant.start()
+    unittest.main()
+    PyAbquant.finish()
